d9c1bebb4b06a564ef32dfe190b9b770ad521e8e
Parker Evans <parkerevans@SUU.EDU>
2019-09-30 20:54:52 -0600

    I commented out our game loop please test

--- Driver.cpp
+++ Driver.cpp
@@ -25,40 +25,49 @@ int main()
 	fillArray();
 	hero_select(h);
 	
+	print_array(h);
+	h = move_hero(h);
+	std::cout<<h.x<<", "<<h.y<<std::endl;
+	print_array(h);
 	
-	while(h.health > 0){
-		print_array(h);
-		h = move_hero(h);
-		int temp = get_tile(h);
-		if(temp == 13)
-		{
-			exit(0);
-		}
-		if(temp == 0)
-		{
-			found_spike(h);
-			// h = take_damage(h);
-		}
-		if(temp == 1){
-			found_qs(h);
-		}
-		if(temp == 2)
-		{
-			found_insult(h);
-		}
-		if(temp == 3)
-		{
-			found_good_potion(h);
-		}
-		if(temp == 4)
-		{
-			found_bad_potion(h);
-		}
-		if(temp == 5)
-		{
-			found_empty(h);
-		}
-	}
+	h = move_hero(h);
+	std::cout<<h.x<<", "<<h.y<<std::endl;
+	print_array(h);
+	
+	
+	// while(h.health > 0){
+		// print_array(h);
+		// h = move_hero(h);
+		// int temp = get_tile(h);
+		// if(temp == 13)
+		// {
+			// exit(0);
+		// }
+		// if(temp == 0)
+		// {
+			// found_spike(h);
+			//h = take_damage(h);
+		// }
+		// if(temp == 1){
+			// found_qs(h);
+		// }
+		// if(temp == 2)
+		// {
+			// found_insult(h);
+		// }
+		// if(temp == 3)
+		// {
+			// found_good_potion(h);
+		// }
+		// if(temp == 4)
+		// {
+			// found_bad_potion(h);
+		// }
+		// if(temp == 5)
+		// {
+			// found_empty(h);
+		// }
+	// }
 	std::cout<<"You win maybe";
 	return 0;
 }
